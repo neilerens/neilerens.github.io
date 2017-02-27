0460fde1dfc7596fe2832e471f3af10caea1a3b0
Neil Rens <neilerens@gmail.com>
2017-02-27 13:47:56 +0100

    navbar, resume, linkedin, about text, portfolio text

--- js/grayscale.js
+++ js/grayscale.js
@@ -6,19 +6,20 @@
 
 // jQuery to collapse the navbar on scroll
 $(window).scroll(function() {
-    if ($(".navbar").offset().top > 50) {
+    if ($(".navbar").offset().top >= 0) {
         $(".navbar-fixed-top").addClass("top-nav-collapse");
     } else {
         $(".navbar-fixed-top").removeClass("top-nav-collapse");
     }
 });
 
+
 // jQuery for page scrolling feature - requires jQuery Easing plugin
 $(function() {
     $('a.page-scroll').bind('click', function(event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
+            scrollTop: $($anchor.attr('href')).offset().top - 50
         }, 1500, 'easeInOutExpo');
         event.preventDefault();
     });
